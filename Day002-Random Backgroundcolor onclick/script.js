99c46847b5a42d793c4db98ca120c22ae6de586a
Vidushika0316 <dasanayakasmvt.19@uom.lk>
2023-03-06 22:33:58 +0530

    day002-random backgroundcolor onclick

---
+++ Day002-Random Backgroundcolor onclick/script.js
@@ -0,0 +1,10 @@
+function changeColor() {
+  document.body.style.backgroundColor =
+    'rgb(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ')';
+}
